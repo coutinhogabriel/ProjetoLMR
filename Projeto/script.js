2d626919c4053e5a61bb9cd4820fbb3db65eec4a
coutinhogabriel <gabriel.coutin@hotmail.com>
2023-06-15 19:24:15 -0300

    projeto

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,12 +1,10 @@
 function inscrever(event) {
-  event.preventDefault(); // Impede o envio do formulário por padrão
-
   var senha = document.getElementById("password").value;
   var confirmarSenha = document.getElementById("confirmpassword").value;
 
   if (senha !== confirmarSenha) {
+    event.preventDefault(); // Impede o envio do formulário quando as senhas não coincidem
     alert("As senhas não são iguais. Por favor, digite novamente.");
-    return; // Interrompe a execução da função sem limpar os dados
   }
-
 }
+
